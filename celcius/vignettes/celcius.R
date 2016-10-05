1f40ee5c9fe78764774c7f70807587c5177e1d32
rebinhosini <rhosini@hotmail.com>
2016-10-05 17:11:15 +0200

    vignette updates

---
+++ celcius/vignettes/celcius.R
@@ -0,0 +1,15 @@
+## ------------------------------------------------------------------------
+#library(celcius)
+#brute_force_knapsack(knapsack_objects[1:8,], W = 3500)
+
+## ------------------------------------------------------------------------
+#library(celcius)
+#knapsack_dynamic(knapsack_objects[1:8,], W = 3500)
+
+## ---- fig.show='hold'----------------------------------------------------
+plot(1:10)
+plot(10:1)
+
+## ---- echo=FALSE, results='asis'-----------------------------------------
+knitr::kable(head(mtcars, 10))
+
