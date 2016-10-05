60fa747bf2e53f273c96a1f2dd3e5519ee541843
Arian Barakat <arianbarakat@c-135-91.eduroam.liu.se>
2016-10-05 11:35:54 +0200

    Finished the greedy_knapsack

--- Draft/Lab6_draft.R
+++ Draft/Lab6_draft.R
@@ -120,7 +120,47 @@ knapsack_dynamic(x = knapsack_objects[1:12,], W = 3500)
 knapsack_dynamic(x = knapsack_objects[1:12,], W = 2000)
 
 
+######################################
+## Greedy knapsack
 
+greedy_knapsack <- function(x, W){
+  if(!is.data.frame(x)){
+    stop("x must be a data.frame")
+  }
+  
+  #Creating the a fractional variable and sorting the
+  # row according to the new variable
+  x$frac <- x$v/x$w
+  x <- x[order(x$frac, decreasing = TRUE), ]
+  
+  #Creating two vectors for holding the result 
+  value <- vector("numeric")
+  elements <- vector("numeric")
+  
+  #Initilazing values
+  weight <- 0
+  value <- 0
+  
+  
+  i <- 1
+  while(weight + x[i,"w"] < W){
+    value <- value + x[i,"v"] 
+    weight <- weight + x[i,"w"]
+    elements[i] <- rownames(x[i,])
+    i <- i + 1
+  }
+  
+  res <- list(
+    "value" = value,
+    "elements" = elements
+  )
+ 
+  return(res)
+   
+}
+
+greedy_knapsack(x = knapsack_objects[1:800,], W = 3500)
+greedy_knapsack(x = knapsack_objects[1:1200,], W = 2000)
 
 ## Help files
 intToBits(matrix(rep(c(0,1),9), ncol = 3))
