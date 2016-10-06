b888fd98c4b7fffe43db944384b4311f829b6fca
rebinhosini <rhosini@hotmail.com>
2016-10-06 13:01:44 +0200

    Did some changes to the testing file

--- celcius/tests/testthat/test_testingfunctions.R
+++ celcius/tests/testthat/test_testingfunctions.R
@@ -5,6 +5,7 @@ install.packages("microbenchmark", repos = "http://cran.rstudio.com/")
 
 library(microbenchmark)
 library(combinat)
+knapsack_objects <- knapsack_data_gen()
 
 ######################################DATA FOR TESTING###############################
 
@@ -15,7 +16,7 @@ context("knapsack")
 #####################################Brute_force_knapsack###############################
 
 test_that("Brute_force_knapsack", {
-  data(knapsack_objects)
+
   expect_that(brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500),
               equals(list(value = 121186, elements = c(6,8))))
   
@@ -48,9 +49,9 @@ test_that("greedy_knapsack", {
   expect_that(greedy_knapsack(x = c(1:3), W = 2000:2001),
               throws_error("W must be of length 1"))
   
-  expect_that(colnames(knapsack_objects2[1:8,])[1],
+  expect_that(colnames(knapsack_objects[1:8,])[1],
               equals("w"))
-  expect_that(colnames(knapsack_objects2[1:8,])[2],
+  expect_that(colnames(knapsack_objects[1:8,])[2],
               equals("v"))
 }
 )
@@ -62,8 +63,13 @@ test_that("knapsack_dynamic", {
   
   expect_that(knapsack_dynamic(x = knapsack_objects[1:8,], W = 3500),
               equals(list(value= 192647, 
-                          elements= c(92, 574, 472, 80, 110, 537, 332, 117, 37, 776, 577, 288, 234, 255, 500, 794, 55,
-                                  290, 436, 346, 282, 764, 599, 303, 345, 300, 243, 43, 747, 35, 77, 229, 719, 564)))
+                          elements= c(92, 574, 472, 80, 110, 537, 332, 117, 37, 
+                                      776, 577, 288, 234, 255, 500, 794, 55,
+                                      290, 436, 346, 282, 764, 599, 303, 345, 
+                                      300, 243, 43, 747, 35, 77, 229, 719, 564)
+                          )
+                     )
+              )
   
   expect_that(knapsack_dynamic(x = knapsack_objects[1:12,], W = 2000),
               equals(list(value = 16922, elements = c(8,9))))
@@ -73,10 +79,10 @@ test_that("knapsack_dynamic", {
   expect_that(knapsack_dynamic(x = c(1:3), W = 2000:2001),
               throws_error("W must be of length 1"))
   
-  expect_that(colnames(knapsack_objects2[1:8,])[1],
+  expect_that(colnames(knapsack_objects[1:8,])[1],
               equals("w"))
-  expect_that(colnames(knapsack_objects2[1:8,])[2],
-              equals("v")))
+  expect_that(colnames(knapsack_objects[1:8,])[2],
+              equals("v"))
 })
 
 
